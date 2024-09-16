b40113a2c3a17d833da10517c18b0b0c0e8137d1
HossainRafi <hossainrafi71@gmail.com>
2024-09-16 22:45:41 +0600

    navigation menu done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-md">
@@ -5,13 +7,54 @@ const Navbar = () => {
       <div className="container max-w-7xl mx-auto px-4 py-3 flex justify-between items-center gap-4">
         {/* logo */}
         <div>
-          <a href="/">
+          <Link to="/">
             <img src="/logo.png" alt="logo" />
-          </a>
+          </Link>
         </div>
 
         {/* menu items */}
-        <div>menu</div>
+        <ul className="md:flex hidden space-x-8 text-gray-700 font-medium">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "text-secondary" : "hover:text-secondary"
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/blogs"
+              className={({ isActive }) =>
+                isActive ? "text-secondary" : "hover:text-secondary"
+              }
+            >
+              Blogs
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? "text-secondary" : "hover:text-secondary"
+              }
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive ? "text-secondary" : "hover:text-secondary"
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
 
         {/* search and theme toggler */}
         <div>search and theme</div>
