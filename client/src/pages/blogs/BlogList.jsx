eface3573c096eaa989da7ea440828c4cdc6693e
HossainRafi <hossainrafi71@gmail.com>
2024-09-17 10:35:07 +0600

    blog card data

--- client/src/pages/blogs/BlogList.jsx
+++ client/src/pages/blogs/BlogList.jsx
@@ -1,27 +1,25 @@
 import { useEffect, useState } from "react";
-
+import BlogCard from "./BlogCard";
 
 const BlogList = () => {
-const [blogs, setBlogs] =useState([])
+  const [blogs, setBlogs] = useState([]);
 
-useEffect(()=>{
-  fetch("blogs.json")
-   .then((response) => response.json())
-   .then((data) => setBlogs(data))
-   .catch((error) => console.error("Error:", error));
-},[])
+  useEffect(() => {
+    fetch("blogs.json")
+      .then((response) => response.json())
+      .then((data) => setBlogs(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
 
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
         {blogs.map((blog, index) => (
-          <div key={index}>
-            <h1>{blog.title}</h1>
-          </div>
+          <BlogCard key={index} blog={blog} />
         ))}
       </div>
     </div>
   );
-}
+};
 
-export default BlogList
+export default BlogList;
