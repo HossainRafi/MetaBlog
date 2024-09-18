449be8885645adbd92cfb16d1f005e8872926f97
HossainRafi <hossainrafi71@gmail.com>
2024-09-18 17:17:21 +0600

    connecting router and controller

--- server/src/routes/blog.routes.js
+++ server/src/routes/blog.routes.js
@@ -1,81 +1,27 @@
 const express = require("express");
+const {
+  getAllBlogs,
+  getBlogById,
+  postANewBlog,
+  deleteABlogById,
+  updateABlogById,
+} = require("../controllers/blog.controllers");
+
 const router = express.Router();
-const Blog = require("../models/blog.model");
 
 // get all blogs
-router.get("/", async (req, res) => {
-  try {
-    const blogs = await Blog.find().sort({ createdAt: -1 });
-    res.status(200).send({ message: "Blogs fetched successfully", blogs });
-  } catch (error) {
-    console.error("Error fetching all blogs", error);
-    res.status(500).send({ message: "Error fetching all blogs", error });
-  }
-});
+router.get("/", getAllBlogs);
 
-// get a single blog by id
-router.get("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const blog = await Blog.findById(id);
-    if (!blog) {
-      return res.status(404).send({ message: "No blog found" });
-    }
-    res.status(200).send({ message: "Blog is fetched successfully", blog });
-  } catch (error) {
-    console.error("Error fetching a blog by id", error);
-    res.status(500).send({ message: "Error fetching a blog by id", error });
-  }
-});
+// get a single blog
+router.get("/:id", getBlogById);
 
 // post a new blog
-router.post("/add-post", async (req, res) => {
-  try {
-    const newBlog = new Blog({ ...req.body });
-
-    const blog = await newBlog.save();
-    res.status(200).send({ message: "Blog posted successfully", blog });
-  } catch (error) {
-    console.error("Error posting a new blog", error);
-    res.status(500).send({ message: "Error posting a new blog", error });
-  }
-});
+router.post("/add-post", postANewBlog);
 
 // delete a blog
-router.delete("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deletedBlog = await Blog.findByIdAndDelete(id);
-    if (!deletedBlog) {
-      return res.status(404).send({ message: "No blog found" });
-    }
-
-    res
-      .status(200)
-      .send({ message: "Blog deleted successfully", blog: deletedBlog });
-  } catch (error) {
-    console.error("Error deleting blog", error);
-    res.status(500).send({ message: "Error deleting blog", error });
-  }
-});
+router.delete("/:id", deleteABlogById);
 
 // update a blog
-router.put("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedBlog = await Blog.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
-    if (!updatedBlog) {
-      return res.status(404).send({ message: "No blog found" });
-    }
-    res
-      .status(200)
-      .send({ message: "Blog updated successfully", blog: updatedBlog });
-  } catch (error) {
-    console.error("Error updating blog", error);
-    res.status(500).send({ message: "Error updating blog", error });
-  }
-});
+router.put("/:id", updateABlogById);
 
 module.exports = router;
