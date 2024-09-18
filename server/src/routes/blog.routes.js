8a33cd7f37ee9b56fd28756dd47830a7575e87e0
HossainRafi <hossainrafi71@gmail.com>
2024-09-18 16:14:44 +0600

    get all blogs

--- server/src/routes/blog.routes.js
+++ server/src/routes/blog.routes.js
@@ -2,16 +2,33 @@ const express = require("express");
 const router = express.Router();
 const Blog = require("../models/blog.model");
 
-router.get("/", (req, res) => {
-  res.send("from blog routes");
+
+// get all blogs
+router.get("/", async (req, res) => {
+  try {
+    const blogs = await Blog.find().sort({ createdAt: -1 });
+    res.status(200).send({ message: "Blogs fetched successfully", blogs });
+  } catch (error) {
+    console.error("Error fetching all blogs", error);
+    res.status(500).send({ message: "Error fetching all blogs", error });
+  }
 });
+
+
+
 
 // post a new blog
 router.post("/add-post", async (req, res) => {
-  const newBlog = new Blog({ ...req.body });
+  try {
+    const newBlog = new Blog({ ...req.body });
 
-  const blog = await newBlog.save();
-  res.status(200).send({message:"Blog posted successfully",blog});
+    const blog = await newBlog.save();
+    res.status(200).send({ message: "Blog posted successfully", blog });
+  } catch (error) {
+    console.error("Error posting a new blog", error);
+    res.status(500).send({ message: "Error posting a new blog", error });
+  }
+  
 });
 
 module.exports = router;
