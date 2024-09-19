96ec124cd1bdf2ca93068b11ddb6046d04efcef9
HossainRafi <hossainrafi71@gmail.com>
2024-09-19 11:05:03 +0600

    search functionality done

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,15 +1,32 @@
+import { useContext, useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { BlogContext } from "../context/BlogContext";
 
 const Search = () => {
+  const { searchTerm, setSearchTerm } = useContext(BlogContext);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSearch = () => {
+    setSearchTerm(inputValue);
+  };
+
   return (
     <div className="relative">
       <input
         type="text"
-        placeholder="Search ....."
+        placeholder="Search..."
+        onChange={handleInputChange}
         className="bg-[#ececec] py-2 pl-4 pr-4  focus:outline-none rounded-full"
       />
       <button>
-        <FaSearch className="absolute right-3 top-2.5 hover:text-secondary" />
+        <FaSearch
+          onClick={handleSearch}
+          className="absolute right-3 top-3 hover:text-secondary"
+        />
       </button>
     </div>
   );
--- client/src/pages/blogs/BlogList.jsx
+++ client/src/pages/blogs/BlogList.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import BlogCard from "./BlogCard";
+import { BlogContext } from "../../context/BlogContext";
 
 const BlogList = () => {
-  const [searchTerm] = useState(""); //TODO: will use blog context
+  const { searchTerm } = useContext(BlogContext); // state for search input value
   const [blogs, setBlogs] = useState([]);
   const [showBlogs, setShowBlogs] = useState(6);
 
